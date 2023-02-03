7de3e4fcf4996345c21ac4eb7e279a26a4ee9430
AzeemJoko <g00382379@atu.ie>
2023-02-03 16:44:28 +0000

    Second commit problem sheet

---
+++ types.js
@@ -0,0 +1,13 @@
+//Basic Types of typescript
+//Boolean
+var isDone = false;
+console.log("Value assigned to isDone: " + isDone);
+//Number
+var decimal = 6;
+console.log("Value assigned to decimal is: " + decimal);
+//String
+var fullName = "Azeem Joko";
+console.log("Hi my full name is: " + fullName);
+//Array
+var list = [1, 2, 3];
+console.log("The value of the list is: " + list);
---
+++ types.ts
@@ -0,0 +1,20 @@
+//Basic Types of typescript
+//Boolean
+let isDone: boolean = false
+console.log("Value assigned to isDone: "+ isDone);
+
+//Number
+let decimal: number = 6;
+console.log("Value assigned to decimal is: "+ decimal);
+
+//String
+let fullName: string = "Azeem Joko";
+console.log("Hi my full name is: "+ fullName);
+
+//Array
+let list: number[] = [1,2,3];
+console.log("The value of the list is: "+ list);
+
+
+
+
